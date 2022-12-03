bf010aa84122e7c413db32ed791f1f245879709f
mooooon333 <742932097@qq.com>
2022-12-02 20:45:29 -0600

    stage 5

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ class Database:
     host = "34.171.35.27"
     user = "root"
     password = "test1234"
-    db = "cs-411"
+    db = "testDB"
     self.con = pymysql.connect(host=host,
                                user=user,
                                password=password,
@@ -28,8 +28,79 @@ class Database:
     return result
 
   def search_user(self, firstName, lastName, email):
-    sql = "select * from testDB.User where firstName = %s or lastName = %s or email = %s"
+    sql = "select * from testDB.User where FirstName = %s or LastName = %s or Email = %s"
+    self.cur.execute(sql, (firstName, lastName, email))
+    result = self.cur.fetchall()
+    return result
+
+  def add_user(self, firstName, lastName, email, password):
+    try:
+      sql = "INSERT INTO testDB.User (FirstName, LastName, Email, Password) VALUES (%s, %s, %s, %s)"
+      self.cur.execute(sql, (firstName, lastName, email, password))
+      self.con.commit()
+    except:
+      print("error!")
+
+  def add_unit(self, buildingID, conditioner, price, numberBedrooms, numberBathrooms, numberKitchens, totalLivingArea):
+      sql = "INSERT INTO testDB.Unit (BuildingID, Conditioner, Price, NumberBedrooms, NumberBathrooms, NumberKitchens, TotalLivingArea) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+      self.cur.execute(sql, (buildingID, conditioner, price, numberBedrooms,
+                             numberBathrooms, numberKitchens, totalLivingArea))
+      self.con.commit()
+    
+  def delete_user(self, firstName, lastName, email):
+    sql = "DELETE FROM testDB.User WHERE Email = %s AND FirstName = %s AND LastName = %s"
+    self.cur.execute(sql, (email, firstName, lastName))
+    self.con.commit()
+
+  def update_user(self, firstName, lastName, email):
+    sql = "UPDATE testDB.User SET Email = %s WHERE FirstName = %s AND LastName = %s"
+    self.cur.execute(sql, (email, firstName, lastName))
+    self.con.commit()
+
+  def advance_query_1(self, built_year, stories, higher_lower):
+    sql = ''
+    stories = int(stories)
+    if higher_lower == "lower":
+      sql = "SELECT b.BuildingID, YearsBuilt, TotalStories, Price FROM testDB.Building b Join testDB.Unit USING(BuildingID) WHERE Price < (SELECT AVG(Price) FROM testDB.Unit) AND YearsBuilt > %s AND TotalStories >= %s LIMIT 15"
+    else:
+      sql = "SELECT b.BuildingID, YearsBuilt, TotalStories, Price FROM testDB.Building b Join testDB.Unit USING(BuildingID) WHERE Price > (SELECT AVG(Price) FROM testDB.Unit) AND YearsBuilt > %s AND TotalStories >= %s LIMIT 15"
+    self.cur.execute(sql, (built_year, stories))
+    self.con.commit()
+    result = self.cur.fetchall()
+    return result
+
+  def advance_query_2(self, reviewYear, house_type, air_condition):
+
+    if air_condition == "on":
+      air_condition = 1
+    else:
+      air_condition = 0
+
+    sql = "SELECT b.BuildingID, Rating, COUNT(Rating) Number_Rating FROM testDB.Building b JOIN testDB.Review USING(BuildingID) WHERE ReviewDate LIKE %s AND BuildingType = %s AND AirConditioning = %s GROUP BY BuildingID, Rating ORDER BY Number_Rating DESC LIMIT 15"
+    reviewYear = reviewYear + "%"
+    self.cur.execute(sql, (reviewYear, house_type, air_condition))
+    self.con.commit()
+    result = self.cur.fetchall()
+    return result
+
+  def simple_discount_1(self, UnitID):
+    sql = "SELECT * FROM testDB.DiscountTable WHERE UnitID = %s"
+    self.cur.execute(sql, (UnitID))
+    self.con.commit()
+    result = self.cur.fetchall()
+    return result
+
+  def call_procedure(self):
+    sql = "call testDB.air_discount()"
     self.cur.execute(sql)
+    self.con.commit()
+    result = self.cur.fetchall()
+    return result
+
+  def login(self, email, password):
+    sql = "SELECT FirstName, LastName FROM testDB.User WHERE Email = %s and Password = %s"
+    self.cur.execute(sql, (email, password))
+    self.con.commit()
     result = self.cur.fetchall()
     return result
 
--- main.py
+++ main.py
@@ -1,28 +1,135 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import db
 
 app = Flask(__name__)
 mydb = db.Database()
+app.secret_key = "cs411"
+
 
 @app.route('/')
 def index():
+  return render_template('index.html')
+
+
+@app.route('/search_user', methods=['POST'])
+def search_user():
+  firstName = request.form.get('fname')
+  lastName = request.form.get('lname')
+  email = request.form.get('email')
+  result = mydb.search_user(firstName, lastName, email)
+  return render_template('users.html', result=result)
+
+
+@app.route('/call_procedure', methods=['POST'])
+def call_procedure():
+  result = mydb.call_procedure()
+  return render_template('users.html', result=result)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_new_user():
+  if request.method == 'POST':
+    firstName = request.form.get('fname1')
+    lastName = request.form.get('lname1')
+    email = request.form.get('email1')
+    password = request.form.get('password1')
+    print(password)
+    mydb.add_user(firstName, lastName, email, password)
     return render_template('index.html')
 
 
-@app.route('/search_user', methods = ['POST'])
-def search_user():
-    firstName = request.form.get('fname')
-    lastName = request.form.get('lname')
-    email = request.form.get('email')
-    result = mydb.search_user(firstName, lastName, email)
-    return render_template('users.html', result = result)
+@app.route('/add_unit', methods=['POST'])
+def add_new_unit():
+  if request.method == 'POST':
+    buildingID = request.form.get('buildingid')
+    conditioner = request.form.get('Conditioner')
+    price = request.form.get('price')
+    numberBedrooms = request.form.get('numBedroom')
+    numberBathrooms = request.form.get('numBathroom')
+    numberKitchens = request.form.get('numkitchen')
+    totalLivingArea = request.form.get('totalLivingArea')
+    mydb.add_unit(buildingID, conditioner, price, numberBedrooms,
+                  numberBathrooms, numberKitchens, totalLivingArea)
+    return render_template('login.html')
+
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+  if request.method == 'POST':
+    firstName = request.form.get('delete_firstname')
+    lastName = request.form.get('delete_lastname')
+    email = request.form.get('delete_email')
+    mydb.delete_user(firstName, lastName, email)
+    return render_template('login.html')
+
+
+@app.route('/update_user', methods=['POST'])
+def update_user():
+  if request.method == 'POST':
+    firstName = request.form.get('update_firstname')
+    lastName = request.form.get('update_lastname')
+    new_email = request.form.get('update_email')
+    mydb.update_user(firstName, lastName, new_email)
+    return render_template('login.html')
+
+
+@app.route('/advance_1', methods=["POST"])
+def get_advance_1():
+  built_year = request.form.get('builtYear')
+  stories = request.form.get('minStories')
+  higher_lower = request.form.get('radio')
+  result = mydb.advance_query_1(built_year, stories, higher_lower)
+  return render_template('login.html',
+                         built_year=built_year,
+                         stories=stories,
+                         higher_lower=higher_lower,
+                         advance_1=result)
+
+
+@app.route('/advance_2', methods=["POST"])
+def get_advance_2():
+  reviewYear = request.form.get('reviewYear')
+  house_type = request.form.get('buildingType')
+  air_condition = request.form.get('ac')
+  result = mydb.advance_query_2(reviewYear, house_type, air_condition)
+
+  return render_template('login.html',
+                         reviewYear=reviewYear,
+                         house_type=house_type,
+                         air_condition=air_condition,
+                         advance_2=result,
+                         firstname=session['firstname'],
+                         lastname=session['lastname'])
+
+
+@app.route('/sim_disc_1', methods=["POST"])
+def get_sim_disc_1():
+  UnitID = request.form.get('UnitID')
+  result = mydb.simple_discount_1(UnitID)
+
+  return render_template('login.html', sim_disc_1=result)
+
+
+@app.route('/login', methods=["POST"])
+def login_user():
+  email = request.form.get("login_email")
+  password = request.form.get("login_password")
+  result = mydb.login(email, password)
+  firstname = None
+  lastname = None
+  if result:
+    firstname = result[0]["FirstName"]
+    lastname = result[0]["LastName"]
+  session['firstname'] = firstname
+  session['lastname'] = lastname
+  return render_template('login.html', firstname=firstname, lastname=lastname)
 
 
 @app.route('/test_db/')
 def test_db():
-    result = mydb.test()
-    return result
+  result = mydb.test()
+  return result
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81, debug=True)
+  app.run(host='0.0.0.0', port=81, debug=True)
